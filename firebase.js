806b72beee21206e8b248d3997022ad82abf71c0
Sarkhan <abdullayevsarxan92@gmail.com>
2023-11-17 00:33:19 +0400

    Implement user authorization logic

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
 import data from "./firebase-data.json" assert { type: 'json' };
-// import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 
 const firebaseConfig = {
@@ -12,6 +12,7 @@ const firebaseConfig = {
   messagingSenderId: "246565311057",
   appId: "1:246565311057:web:238b3bfd743a3449841e70"
 };
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
@@ -19,24 +20,31 @@ const auth = getAuth(app);
 const loadFirestoreData = async () => {
   try {
     const accountsCollection = collection(db, 'accounts');
-    const roomsCollection = collection(db, 'rooms');
+  
         
-    for (const [user, userData] of Object.entries(data.Accounts)) {
-      await addDoc(accountsCollection, { user, ...userData });
-      // await createUserWithEmailAndPassword(auth, user, userData.password);
-    }
-
-    for (const roomData of data.Rooms) {
-      await addDoc(roomsCollection, roomData);
-    }
-
+     await Promise.all(
+        Object.entries(data.Accounts).map(async ([user, userData]) => {
+        const { password, image } = userData;
+        await addDoc(accountsCollection, { user, password, image });
+        await createUserWithEmailAndPassword(auth, `${user}@example.com`, `123${password}`);
+      })
+    );
+        
     console.log('Data loaded into Firestore successfully');
   } catch (error) {
     console.error('Error loading data into Firestore:', error);
   }
+
+  try {
+    const roomsCollection = collection(db, 'rooms');
+      data.Rooms.map(async (roomData) => {
+      await addDoc(roomsCollection, roomData);
+    })
+  } catch (error) {
+    console.error('Error loading data into Firestore:', error);
+  }
+
 };
 
 loadFirestoreData();
-
-export { app, db, auth };
-
+export { app, db };
